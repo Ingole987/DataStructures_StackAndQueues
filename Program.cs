aff7c902bf722e778b022f3daf49faae3c91a51c
Ingole987 <pratik.ingole98@gmail.com>
2022-01-30 16:28:23 +0530

    added:remProgram

--- Program.cs
+++ Program.cs
@@ -2,13 +2,21 @@
 using StacksAndQueues;
 
 Console.WriteLine("Hello World!");
-StackUsingLinkedList linkedListStack = new StackUsingLinkedList();
-linkedListStack.Push(70);
-linkedListStack.Push(30);
-linkedListStack.Push(56);
-linkedListStack.Display();
+//StackUsingLinkedList linkedListStack = new StackUsingLinkedList();
+//linkedListStack.Push(70);
+//linkedListStack.Push(30);
+//linkedListStack.Push(56);
+//linkedListStack.Display();
 //linkedListStack.Peek(); top.data
 //linkedListStack.Pop();
 //linkedListStack.IsEmpty();
 //linkedListStack.Display();
+Queue linkedListQueues = new Queue();
+linkedListQueues.Enqueue(56);
+linkedListQueues.Enqueue(30);
+linkedListQueues.Enqueue(70);
+linkedListQueues.Display();
+Console.WriteLine("------------------");
+linkedListQueues.Dequeue();
+linkedListQueues.Display();
 Console.ReadKey();
